24c51db09d1c1786f815863643db28793129b37e
Devesh <deveshsaini594@gmail.com>
2024-09-21 15:41:22 +0530

    onclick event and function call

--- myapp/src/Users.js
+++ myapp/src/Users.js
@@ -7,12 +7,12 @@
 // }
 // export default Users;
 
-import React, { Component } from "react";
-export default class Users extends Component {
-  render() {
-    return <h1>devesh katariya</h1>;
-  }
-}
+// import React, { Component } from "react";
+// export default class Users extends Component {
+//   render() {
+//     return <h1>devesh katariya</h1>;
+//   }
+// }
 
 // import React, { Component, createElement } from "react";
 // export default class Users extends Component {
@@ -24,3 +24,35 @@ export default class Users extends Component {
 //     );
 //   }
 // }
+
+function App() {
+  function apple() {
+    alert("funcation called");
+  }
+  return (
+    <div>
+      <h1>devesh katariya</h1>
+
+      <button onClick={apple}>click me</button>
+      {/* <button onClick={() => apple()}>click me</button> */}
+    </div>
+  );
+}
+export default App;
+
+// function App() {
+//   let data = "komal";
+//   function apple() {
+//     data = "devesh";
+//     alert(data);
+//   }
+//   return (
+//     <div>
+//       <h1>devesh katariya</h1>
+//       <h1>{data}</h1>
+//       <button onClick={apple}>click me</button>
+//       {/* <button onClick={() => apple()}>click me</button> */}
+//     </div>
+//   );
+// }
+// export default App;
