c17622af86d5fb3daf39981a6ac3db6dc4feebc3
Devesh <deveshsaini594@gmail.com>
2024-09-21 15:15:04 +0530

    function and class component

---
+++ myapp/src/Users.js
@@ -0,0 +1,26 @@
+// function Users() {
+//   return (
+//     <div className="App">
+//       <h1>hello world hii devesh</h1>
+//     </div>
+//   );
+// }
+// export default Users;
+
+import React, { Component } from "react";
+export default class Users extends Component {
+  render() {
+    return <h1>devesh katariya</h1>;
+  }
+}
+
+// import React, { Component, createElement } from "react";
+// export default class Users extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       React.createElement("h1", null, "hello katariya")
+//     );
+//   }
+// }
